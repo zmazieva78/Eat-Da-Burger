e0a14794653de637f1ec21f12346a24534328245
Zarina Mazieva <zmazieva78@gmail.com>
2019-01-30 23:01:58 -0800

    fixed default route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,11 @@ var burger = require('../models/burger.js');
 
 // Index redirect
 router.get('/', function(req, res) {
-	res.redirect('/index');
+	//res.redirect('/index');
+	burger.selectAll(function(data) {
+		var hbsObject = {burgers: data};
+		res.render('index', hbsObject);
+	});
 });
 
 
